5f4555129988e68153fbc14bb661ef1f5928a5f7
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-05-01 14:29:41 +0200

    Clean up code

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,13 +4,13 @@ import * as config from '../config/app.json';
 
 const app: express.Application = express();
 
-// Cross-origin resource sharing
-app.use(cors());
+/* Config */
+app.set('port', process.env.PORT || config.port);
+app.use(cors()); // Cross-origin resource sharing
+
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello! This site will be a tinder app for developers. Stay tuned!');
 });
 
-app.listen(config.port, () => {
-  console.log(`Listening on port ${config.port}`);
-});
+app.listen(() => console.log(`Listening on port ${config.port}`));
