ddbb5800e2b64b8dc44ebd72cc8ea93dcf21f677
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-05-01 15:51:34 +0200

    Create database config

---
+++ server/config/db.ts
@@ -0,0 +1,27 @@
+import { config } from 'dotenv';
+
+config({ path: './../.env' });
+
+export default {
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  test: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  production: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+};
--- server/src/app.ts
+++ server/src/app.ts
@@ -13,4 +13,4 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello! This site will be a tinder app for developers. Stay tuned!');
 });
 
-app.listen(() => console.log(`Listening on port ${config.port}`));
+app.listen(app.get('port'), () => console.log(`Listening on port ${config.port}`));
