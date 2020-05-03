a0892962ba527acf94ce8605b700d4bba543d11b
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-05-03 17:01:42 +0200

    Create migration files

---
+++ server/sequelize/migrations/20200501142528-User.js
@@ -0,0 +1,86 @@
+const { v1: uuidv1 } = require('uuid');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('User', {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: uuidv1(),
+      primaryKey: true,
+    },
+    disabled: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+
+    // Personal details
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    gender: {
+      type: Sequelize.ENUM('M', 'F'),
+      allowNull: false,
+    },
+    age: {
+      type: Sequelize.DATEONLY,
+      allowNull: false,
+    },
+    bio: Sequelize.STRING(500),
+    occupation: Sequelize.ARRAY(Sequelize.STRING),
+    education: Sequelize.ARRAY(Sequelize.STRING),
+
+    // Geolocation
+    position: Sequelize.GEOMETRY('POINT'), // Requires PostGIS extension
+
+    // Uploads
+    photo1: Sequelize.STRING,
+    photo2: Sequelize.STRING,
+    photo3: Sequelize.STRING,
+    photo4: Sequelize.STRING,
+    photo5: Sequelize.STRING,
+    photo6: Sequelize.STRING,
+
+    // Settings
+    minAge: {
+      type: Sequelize.INTEGER,
+      defaultValue: 18,
+    },
+    maxAge: {
+      type: Sequelize.INTEGER,
+      defaultValue: 55,
+    },
+    maxDistance: {
+      type: Sequelize.INTEGER,
+      defaultValue: 30,
+    },
+    distanceType: Sequelize.ENUM('km', 'mi'),
+
+    // OAuth stuff should go here
+    refreshToken: Sequelize.STRING,
+
+    // Timestamps
+    createdAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      onUpdate: Sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
+  },
+  {
+    // Should not be unique - Needs fix
+    uniqueKeys: {
+      users: {
+        customIndex: true,
+        fields: ['disabled', 'position', 'gender', 'age'],
+      },
+    },
+  }),
+
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('User'),
+};
