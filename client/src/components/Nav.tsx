ff5d4c0970f6750baf0973b1f23dc1d9153ff6cb
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-05-05 17:14:57 +0200

    Use material UI's makeStyles function instead of storing styles in variables #12

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -6,9 +6,7 @@ import {
   BsThreeDots,
   BsFillPersonFill,
 } from 'react-icons/bs';
-import { MdAccountCircle, MdSettings } from 'react-icons/md';
-import { RiAccountCircleLine } from 'react-icons/ri';
-import { FaCode } from 'react-icons/fa';
+import { MdSettings } from 'react-icons/md';
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import {
   Link,
@@ -21,79 +19,89 @@ import { MenuList, MenuItem, Grid, AppBar, Toolbar } from '@material-ui/core';
 
 import { darkTheme, lightTheme } from '../theme';
 
-export const Nav: React.FC = () => {
-  const [darkMode, setDarkMode] = useState(true);
-  const leftIcon = {
+const useStyles = makeStyles({
+  root: {
+    background: '#424242',
+    lineHeight: '0',
+    position: 'fixed',
+  },
+  leftNavIcon: {
     color: 'white',
     transform: 'scale(1.7)',
     marginTop: '3 px',
-  };
-  const logo = {
+    lineHeight: '0',
+  },
+
+  logo: {
     color: 'red',
     transform: 'scale(2)',
-  };
-
-  const rightIcon = {
+    lineHeight: '0',
+  },
+  rightNavIcon: {
     color: 'white',
     transform: 'scale(1.5)',
-  };
+    lineHeight: '0',
+  },
+});
 
+export const Nav: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const classes = useStyles();
   const { pathname } = useLocation();
 
   let leftLink;
   if (pathname === '/profile') {
     leftLink = (
-      <Link to="/settings" style={rightIcon}>
+      <Link to="/settings" className={classes.leftNavIcon}>
         <MdSettings />
       </Link>
     );
   } else if (pathname === '/chat') {
     leftLink = (
-      <Link to="/settings" style={rightIcon}>
+      <Link to="/settings" className={classes.rightNavIcon}>
         <BsThreeDots />
       </Link>
     );
   } else {
     leftLink = (
-      <Link to="/chats" style={rightIcon}>
+      <Link to="/chats" className={classes.rightNavIcon}>
         <BsChatDotsFill />
       </Link>
     );
   }
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <nav>
-        <AppBar position="static" style={{ background: '#424242' }}>
-          <Toolbar>
-            <Grid
-              container
-              direction="row"
-              justify="space-between"
-              alignItems="center"
-            >
-              <Grid item>
-                <MenuItem style={{ lineHeight: '0' }}>
-                  <Link to="/profile" style={leftIcon}>
-                    <BsFillPersonFill />
-                  </Link>
-                </MenuItem>
-              </Grid>
-              <Grid item>
-                <MenuItem style={{ lineHeight: '0' }}>
-                  <Link to="/#" style={logo}>
-                    <BsCodeSlash />
-                  </Link>
-                </MenuItem>
-              </Grid>
-              <Grid item>
-                <MenuItem style={{ lineHeight: '0' }}>{leftLink}</MenuItem>
-              </Grid>
+    <nav>
+      <AppBar className={classes.root}>
+        <Toolbar>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid item>
+              <MenuItem>
+                <Link to="/profile" className={classes.leftNavIcon}>
+                  <BsFillPersonFill />
+                </Link>
+              </MenuItem>
             </Grid>
-          </Toolbar>
-        </AppBar>
-      </nav>
-    </ThemeProvider>
+            <Grid item>
+              <MenuItem>
+                <Link to="/#" className={classes.logo}>
+                  <BsCodeSlash />
+                </Link>
+              </MenuItem>
+            </Grid>
+            <Grid item>
+              <MenuItem>{leftLink}</MenuItem>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </nav>
   );
 };
 
