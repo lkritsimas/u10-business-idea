a49c5266e19f3f252e973de2c4ab36dd8792ebdd
nbdehlen <nbdehlen@gmail.com>
2020-05-08 16:09:03 +0200

    Fix responsiveness and theme color of ProfileCard and Nav together

--- client/src/utilities/theme.tsx
+++ client/src/utilities/theme.tsx
@@ -2,8 +2,29 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
 
 
-export const theme = (props: string): ThemeOptions => createMuiTheme({
-  palette: {
-    type: props === 'dark' ? 'dark' : 'light',
-  },
-});
+export const theme = (props: string): ThemeOptions => {
+  if (props === 'dark') {
+    return (
+      createMuiTheme({
+        palette: {
+          type: 'dark',
+          background: {
+            paper: '#424242',
+            default: '#424242',
+          },
+        },
+      })
+    );
+  }
+  return (
+    createMuiTheme({
+      palette: {
+        type: 'light',
+        background: {
+          paper: '#fafafa',
+          default: '#fafafa',
+        },
+      },
+    })
+  );
+};
