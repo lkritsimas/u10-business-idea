7f0a189cb2591d48b3cae52d09cd1c180a958d2d
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-05-13 13:34:49 +0200

    Passport initial setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,12 @@ import dotenv from 'dotenv';
 import * as config from '../config/app.json';
 import routes from './routes';
 
+// test!
+const authRoutes = require('./routes/Auth');
+
 dotenv.config();
+const passportSetup = require('../config/passport_setup');
+
 const app: express.Application = express();
 
 /* Config */
@@ -15,9 +20,13 @@ app.use(express.json()); // Parse incoming JSON
 /* API */
 app.use('/api/v1', routes);
 
+// test!
+app.use('/auth', authRoutes);
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello! This site will be a tinder app for developers. Stay tuned!');
 });
 
-
-app.listen(app.get('port'), () => console.log(`Listening on port ${config.port}`));
+app.listen(app.get('port'), () =>
+  console.log(`Listening on port ${config.port}`)
+);
